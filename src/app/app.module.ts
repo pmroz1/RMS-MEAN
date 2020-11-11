5e74452286f7e6a7418e73b1e79124966c363cae
Piotr <p.mroz20@outlook.com>
2020-11-11 23:53:01 +0100

    started working on default layout component and dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { DefaultComponent } from './layouts/default/default.component';
+
+// import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    DefaultComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
+    // MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
